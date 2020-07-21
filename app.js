8de1981bcc84fb1061a0a9d3612fea1ab1ea9607
danielpinto01 <36775347+danielpinto01@users.noreply.github.com>
2020-07-21 10:48:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1>--- Pruebas de rendimiento v1.0</h1><p>Hola mundo</p><p>v1.0.1</p>');
+  res.send('<h1>---1 Pruebas de rendimiento v1.0</h1><p>Hola mundo</p><p>v1.0.1</p>');
 });
 
 app.listen(3000, function () {
