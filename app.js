ccf2e554eaea30f04a068d0659d5ca4a0f2a7afd
danielpinto01 <daniel.pinto01@uptc.edu.co>
2020-07-26 17:50:54 -0500

    Se agrego el timeout

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1>---1 Pruebas de rendimiento v1.0</h1><p>Hola mundo</p><p>v1.0.1</p>');
+  setTimeout(() => {
+    res.send('<h1>---1 Pruebas de rendimiento v1.0</h1><p>Hola mundo</p><p>v1.0.1</p>');
+  }, Math.floor(Math.random() * 10000));
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
